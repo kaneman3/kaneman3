f7c1172cf8a28a66b0de256a1135f886c4291456
kaneman3 <52613642+kaneman3@users.noreply.github.com>
2022-06-05 11:15:42 +0900

    Update forms.py

--- lambdatest/forms.py
+++ lambdatest/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class InputParam(forms.Form):
     Param1 = forms.CharField(max_length=10, label='x')
     Param2 = forms.CharField(max_length=10, label='y')
-    Param3 = forms.CharField(max_length=100, label='comment')
+    Param3 = forms.CharField(max_length=100, label='コメント')
 
